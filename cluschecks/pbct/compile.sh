bc5101c8fe58423af59cd0c8de6ef5d317ab7d36
camilleecu <cuwenhan@outlook.com>
2025-08-03 18:55:33 +0200

    clean out unnecessary

--- cluschecks/pbct/compile.sh
+++
@@ -1,3 +0,0 @@
-cd ~/CLUSPBCTDIR/
-javac -d "bin" -cp ".:jars/commons-math-1.0.jar:jars/jgap.jar" clus/Clus.java
-cd ~/PCT/cluschecks/pbct
--- cluschecks/pbct/main.sh
+++
@@ -1,1 +0,0 @@
-java -cp "$HOME/CLUSPBCTDIR/bin:$HOME/CLUSPBCTDIR/jars/commons-math-1.0.jar:$HOME/CLUSPBCTDIR/jars/jgap.jar" clus.Clus cellcycle_FUN.s
